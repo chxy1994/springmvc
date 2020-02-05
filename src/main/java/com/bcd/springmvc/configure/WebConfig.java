0ea233f0484e489ed71d1c43a6aa88fda4fe4889
chxy1994 <chxy.s@qq.com>
2020-02-05 11:39:25 +0800

    json

---
+++ src/main/java/com/bcd/springmvc/configure/WebConfig.java
@@ -0,0 +1,37 @@
+/*
+author:chxy
+data:2020/2/4
+description:
+*/
+package com.bcd.springmvc.configure;
+
+
+import com.bcd.springmvc.token.HeaderTokenInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getTokenHeader())
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/robots.txt");
+    }
+
+    //token 在header的拦截器
+    @Bean
+    public HandlerInterceptor getTokenHeader(){
+        return new HeaderTokenInterceptor();
+    }
+
+
+}
+
+
+
--- src/main/java/com/bcd/springmvc/controller/UserController.java
+++ src/main/java/com/bcd/springmvc/controller/UserController.java
@@ -9,15 +9,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
-    @RequestMapping("user/select.action")
+    @RequestMapping("/user/select.action")
     public @ResponseBody UserBean select(@RequestBody UserBean bean){
 
         bean.setName("hello");
         return bean;
+    }
+
+    @RequestMapping("/other/login.action")
+    public String login(){
+
+        System.out.println("hello");
+        return "success";
     }
 }
 
